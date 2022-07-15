65c61591df32b492e3f2f05d017c525c19cad38d
CodingWithImran <CodingWithImran@gmail.com>
2022-07-15 10:52:51 +0500

    constructor create and create state in constructor to add variable for sample json value. Display items through loops

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// 5d79a230f6ad4ea5a067d4fab4bd7e57
+// https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=5d79a230f6ad4ea5a067d4fab4bd7e57
+
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar'
 import News from './Components/News'
--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 export default class NewsItems extends Component {
   render() {
+    let {title, description, imagUrl} = this.props;
     return (
-      <div>NewsItems</div>
+      <>
+            <div className="card" style={{borderRadius: "30px"}}>
+              <img src={imagUrl} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+      </>
     )
   }
 }
