868e406343aefc957afef3d2198a4c8233e6dec4
CodingWithImran <CodingWithImran@gmail.com>
2022-07-15 07:27:41 +0500

    Create Navbar and Some components in News App

---
+++ src/Components/News.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export default class News extends Component {
+  render() {
+    return (
+      <>
+          <NewsItems />
+          <NewsItems />
+          <NewsItems />
+          <NewsItems />
+          <NewsItems />
+          <NewsItems />
+          <NewsItems />
+      </>
+
+    )
+  }
+}
